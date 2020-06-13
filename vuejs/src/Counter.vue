d7d0fed0f84b8f541f63298bbe03d3c66166131a
Arnaud Fougerousse <arnaudfgr@gmail.com>
2020-06-13 16:31:07 +0200

    Vue-CLI

---
+++ vuejs/src/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="counter">
+    {{ count }}
+    <div class="counter__button" @click="increment">+</div>
+  </div>
+</template>
+
+<script>
+import store from './store/NotificationsStore'
+  export default{
+    data () {
+      return {
+        state: store.state
+      }
+    },
+    computed:{
+      count (){ return this.state.count}
+    },
+    methods: {
+      increment () {
+        store.increment()
+      }
+    }
+  }
+</script>
+
+<style>
+  .counter{
+    font-weight: bold;
+    width: 80px;
+    height: 80px;
+    text-align: center;
+    background-color: #dfdfdf;
+  }
+</style>
